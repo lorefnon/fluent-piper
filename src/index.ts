b2c2a292ff00696a0d6c17881f3acbc5e24fe789
lorefnon <lorefnon@gmail.com>
2022-07-20 02:27:48 +0530

    Use explicit interfaces so that typescript does not generate an anonymous type for fixed number of steps and falls back to any after that

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,41 @@ type Resolved<T> = T extends Promise<infer ResultT> ? ResultT : T;
 type AnyToUnknown<T> = unknown extends T ? unknown : T;
 type IsNever<T, LeftT, RightT> = AnyToUnknown<T> extends never ? LeftT : RightT;
 
-export const pipe = <T>(val: T) => ({
+export interface PipeStep<T> {
+  value: T;
+  thru: <T2>(op: (i: T) => T2) => PipeStep<T2>;
+  thruAsync: <T2>(op: (i: Resolved<T>) => T2) => PipeStep<Promise<Resolved<T2>>>;
+}
+
+export const pipe = <T>(val: T): PipeStep<T> => ({
   value: val,
   thru: <T2>(op: (i: T) => T2) => pipe(op(val)),
   thruAsync: <T2>(op: (i: Resolved<T>) => T2) =>
     pipe<Promise<Resolved<T2>>>(Promise.resolve<any>(val).then<any>(op)),
 });
 
+export interface PipeFn<InputT = never, OutputT = never> {
+  fn: IsNever<InputT, () => undefined, (i: InputT) => OutputT>;
+  thru: <NextOutputT, StepInputT extends IsNever<InputT, any, OutputT>>(
+    op: ((i?: StepInputT) => NextOutputT) | ((i: StepInputT) => NextOutputT)
+  ) => PipeFn<IsNever<InputT, StepInputT, InputT>, NextOutputT>;
+}
+
 export const pipeFn = <InputT = never, OutputT = never>(
   fns: ((i?: any) => any)[] = []
-) => ({
+): PipeFn<InputT, OutputT> => ({
   get fn() {
-    return (fns.length === 0
-      ? () => undefined
-      : (i: InputT): OutputT => {
-          let result: any = i;
-          for (const fn of fns) {
-            result = fn(result);
+    return (
+      fns.length === 0
+        ? () => undefined
+        : (i: InputT): OutputT => {
+            let result: any = i;
+            for (const fn of fns) {
+              result = fn(result);
+            }
+            return result;
           }
-          return result;
-        }) as IsNever<InputT, () => undefined, (i: InputT) => OutputT>;
+    ) as any;
   },
   thru<NextOutputT, StepInputT extends IsNever<InputT, any, OutputT>>(
     op: ((i?: StepInputT) => NextOutputT) | ((i: StepInputT) => NextOutputT)
@@ -33,23 +48,46 @@ export const pipeFn = <InputT = never, OutputT = never>(
   },
 });
 
+export interface PipeFnAsync<InputT = never, OutputT = never> {
+  fn: IsNever<
+    InputT,
+    () => undefined,
+    (i: InputT) => Promise<Resolved<OutputT>>
+  >;
+  thru: <
+    NextOutputT,
+    StepInputT extends IsNever<InputT, any, Resolved<OutputT>>
+  >(
+    op: ((i?: StepInputT) => NextOutputT) | ((i: StepInputT) => NextOutputT)
+  ) => PipeFnAsync<IsNever<InputT, StepInputT, InputT>, Resolved<NextOutputT>>;
+}
+
 export const pipeFnAsync = <InputT = never, OutputT = never>(
   fns: ((i?: any) => any)[] = []
-) => ({
+): PipeFnAsync<InputT, OutputT> => ({
   get fn() {
-    return (fns.length === 0
-      ? async () => undefined
-      : async (i: InputT): Promise<OutputT> => {
-          let result: any = i;
-          for (const fn of fns) {
-            result = await fn(result);
+    return (
+      fns.length === 0
+        ? async () => undefined
+        : async (i: InputT): Promise<OutputT> => {
+            let result: any = i;
+            for (const fn of fns) {
+              result = await fn(result);
+            }
+            return result;
           }
-          return result;
-        }) as IsNever<InputT, () => undefined, (i: InputT) => Promise<OutputT>>;
+    ) as IsNever<
+      InputT,
+      () => undefined,
+      (i: InputT) => Promise<Resolved<OutputT>>
+    >;
   },
-  thru<NextOutputT, StepInputT extends IsNever<InputT, any, Resolved<OutputT>>>(
+  thru: <
+    NextOutputT,
+    StepInputT extends IsNever<InputT, any, Resolved<OutputT>>
+  >(
     op: ((i?: StepInputT) => NextOutputT) | ((i: StepInputT) => NextOutputT)
-  ) {
+  ) => {
     return pipeFnAsync<
       IsNever<InputT, StepInputT, InputT>,
       Resolved<NextOutputT>
